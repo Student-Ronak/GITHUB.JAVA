064ca51c14bb374a1d1d7665624f397db95cc936
Student-Ronak <rj192169@gmail.com>
2024-05-28 12:02:16 +0530

    Employee Management System Projects

--- src/EmployeeManagementSystem/java/Conn.java
+++ src/EmployeeManagementSystem/java/Conn.java
@@ -27,4 +27,33 @@ public class Conn {
 
     }
 }
+//DATABASE CODE
+//FIRST WE CREATE A DATABASE  WITH  NAME EMS
+//USE THIS COMMAND
+//CREATE DATABASE EMS;
+//    use ems;
+
+//    create table empdetails( empID	varchar(30),
+//    name	varchar(20),
+//    fname	varchar(20),
+//    dob	varchar(30),
+//    salary	varchar(20),
+//    address	varchar(40),
+//    email	varchar(39),
+//    phone	varchar(40),
+//    addhar	varchar(20),
+//    desi	varchar(30),
+//    educ	varchar(30));
+//
+//        create table login(username	varchar(30),
+//        password	varchar(20));
+//
+//insert into login(username,password) values("admin","12345");
+
+
+
+
+
+
+
 
